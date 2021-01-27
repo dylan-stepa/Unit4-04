1ec5ee1e7e5f87b3a83d91067c0200ba6355a550
dylanstepa <dylan.stepa@ocsbstudent.ca>
2021-01-27 14:40:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:powderblue;">
+<h2>Hello</h2>
+
+<button onclick="myFunction()">press it</button>
+
+<script>
+function myFunction() {
+  alert("Good bye");
+}
+</script>
+
+</body>
+</html>
